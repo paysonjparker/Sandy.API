646dedc6bf736e334e720642ca69f46b815bd8b8
Payson Parker <paysonparker@costco.com>
2023-09-05 09:04:53 -0700

    Formula for calculating Handicap Index

--- Sandy/Data/DataTransferObjects/ScoreDto/AddScoreRequest.cs
+++ Sandy/Data/DataTransferObjects/ScoreDto/AddScoreRequest.cs
@@ -3,6 +3,7 @@
     public class AddScoreRequest
     {
         public int Total { get; set; }
+        public float Differential { get; set; }
         public Guid GolferId { get; set; }
     }
 }
--- Sandy/Data/DataTransferObjects/ScoreDto/Score.cs
+++ Sandy/Data/DataTransferObjects/ScoreDto/Score.cs
@@ -4,6 +4,7 @@
     {
         public Guid Id { get; set; }
         public int Total { get; set; }
+        public float Differential { get; set; }
         public Guid GolferId { get; set; }
     }
 }
