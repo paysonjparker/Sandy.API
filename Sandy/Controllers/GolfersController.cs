e254c299318b7ba1e46cadf0ebcb1efa33632d95
Payson Parker <paysonparker@costco.com>
2023-09-05 10:33:13 -0700

    Added controllers

---
+++ Sandy/Controllers/GolfersController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sandy.Business;
+using Sandy.Data;
+using Sandy.Models.DataTransferObjects.GolferDto;
+
+namespace Sandy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("AllowAngularOrigins")]
+    public class GolfersController : ControllerBase
+    {
+        private GolferService _golferService;
+
+        public GolfersController(SandyDbContext dbContext)
+        {
+            this._golferService = new GolferService(dbContext);
+        }
+
+        [HttpPost]
+        public IActionResult AddGolfer(AddGolferRequest addGolferRequest)
+        {
+            var golfer = _golferService.AddGolfer(addGolferRequest);
+
+            return Ok(golfer);
+        }
+
+        [HttpGet]
+        public IActionResult GetAllGolfers()
+        {
+            var golfers = _golferService.GetAllGolfers();
+
+            golfers= golfers.OrderBy(x => x.Name).ToList();
+
+            return Ok(golfers);
+        }
+
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetGolferById(Guid id) 
+        {
+            var golfer = _golferService.GetGolferById(id);
+
+            if(golfer != null)
+            {
+                return Ok(golfer);
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public IActionResult UpdateGolfer(Guid id, UpdateGolferRequest updateGolferRequest)
+        {
+            var exisitngGolfer = _golferService.UpdateGolfer(id, updateGolferRequest);
+
+            if (exisitngGolfer != null)
+            {
+                return Ok(exisitngGolfer);
+            }
+
+            return BadRequest();
+        }
+
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public IActionResult DeleteGolfer(Guid id)
+        {
+            var existingGolfer = _golferService.DeleteGolfer(id);
+
+            if (existingGolfer != false)
+            {
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+    }
+}
---
+++ Sandy/Controllers/ScoresController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Sandy.Business;
+using Sandy.Data;
+using Sandy.Models.DataTransferObjects.ScoreDto;
+
+namespace Sandy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("AllowAngularOrigins")]
+    public class ScoresController : ControllerBase
+    {
+        private ScoreService _scoreService;
+
+        public ScoresController(SandyDbContext dbContext)
+        {
+            this._scoreService = new ScoreService(dbContext);
+        }
+
+        [HttpPost]
+        public IActionResult AddScore(AddScoreRequest addScoreRequest)
+        {
+            var score = _scoreService.AddScore(addScoreRequest);
+
+            return Ok(score);
+        }
+
+        [HttpGet]
+        [Route("{golferId:Guid}")]
+        public IActionResult GetAllScoresByGolfer(Guid golferId)
+        {
+            var scores = _scoreService.GetAllScoresByGolfer(golferId);
+
+            scores = scores.OrderByDescending(x => x.Id).ToList();
+
+            return Ok(scores);
+        }
+
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public IActionResult DeleteScore(Guid id)
+        {
+            var existingScore = _scoreService.DeleteScore(id);
+
+            if (existingScore != false)
+            {
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+    }
+}
